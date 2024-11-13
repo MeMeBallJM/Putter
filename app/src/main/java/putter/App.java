be6a982c04e5862a3dae0b88112f115322466584
MeMeBallJM <morton.l.joshua@gmail.com>
2024-11-12 22:53:38 -0500

    Ping pong

--- app/src/main/java/putter/App.java
+++ app/src/main/java/putter/App.java
@@ -3,12 +3,22 @@
  */
 package putter;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(String[] args) throws Exception {
+        String BOT_TOKEN = System.getenv("BOT_TOKEN");
+
+        JDABuilder.createDefault(BOT_TOKEN) // enable all default intents
+                .enableIntents(GatewayIntent.GUILD_MEMBERS) // also enable privileged intent
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .addEventListeners(new MyListener())
+                .build();
+
     }
 }
